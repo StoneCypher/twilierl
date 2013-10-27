770aff68c93dd180b80940625e99880717751b8b
John Haugeland <stonecypher@gmail.com>
2013-10-26 21:01:14 -0700

    :white_check_mark: works

--- twilierl.erl
+++ twilierl.erl
@@ -7,7 +7,7 @@
 
 -export([
 
-    send/7
+    send/6
 
 ]).
 
@@ -15,41 +15,39 @@
 
 
 
-send(AccountSid, User, Pass, SenderNumber, TargetNumber, Message, MediaUrl) when is_integer(AccountSid) ->
+send(AccountSid, AuthToken, SenderNumber, TargetNumber, Message, MediaUrl) when is_integer(AccountSid) ->
 
-    send(integer_to_list(AccountSid), User, Pass, SenderNumber, TargetNumber, Message, MediaUrl);
-
-
-
-
-
-send(AccountSid, User, Pass, SenderNumber, TargetNumber, Message, MediaUrl) when is_integer(SenderNumber) ->
-
-    send(AccountSid, User, Pass, integer_to_list(SenderNumber), TargetNumber, Message, MediaUrl);
+    send(integer_to_list(AccountSid), AuthToken, SenderNumber, TargetNumber, Message, MediaUrl);
 
 
 
 
 
-send(AccountSid, User, Pass, SenderNumber, TargetNumber, Message, MediaUrl) when is_integer(TargetNumber) ->
+send(AccountSid, AuthToken, SenderNumber, TargetNumber, Message, MediaUrl) when is_integer(SenderNumber) ->
 
-    send(AccountSid, User, Pass, SenderNumber, integer_to_list(TargetNumber), Message, MediaUrl);
-
-
+    send(AccountSid, AuthToken, integer_to_list(SenderNumber), TargetNumber, Message, MediaUrl);
 
 
 
-send(AccountSid, User, Pass, SenderNumber, TargetNumber, Message, MediaUrl) ->
+
+
+send(AccountSid, AuthToken, SenderNumber, TargetNumber, Message, MediaUrl) when is_integer(TargetNumber) ->
+
+    send(AccountSid, AuthToken, SenderNumber, integer_to_list(TargetNumber), Message, MediaUrl);
+
+
+
+
+
+% todo mediaurl support
+
+send(AccountSid, AuthToken, SenderNumber, TargetNumber, Message, _MediaUrl) ->
 
     inets:start(),
 
-    Target = "https://api.twilio.com/2010-04-01/Accounts/" ++ AccountSid ++ "/Messages",
+    Target = "https://" ++ AccountSid ++ ":" ++ AuthToken ++ "@api.twilio.com/2010-04-01/Accounts/" ++ AccountSid ++ "/Messages.json",
+    Args   = "From="  ++ http_uri:encode(SenderNumber) ++
+             "&To="   ++ http_uri:encode(TargetNumber) ++
+             "&Body=" ++ http_uri:encode(Message),
 
-    Target.
-
-%    case httpc:request(post, Target) of
-%        
-%        {ok,{{_,RCode,_},_,Ret}} -> {RCode, Ret};
-%        Other                    -> {error, Other}
-%
-%    end.
+    httpc:request(post, {Target, [], "application/x-www-form-urlencoded", Args}, [], []).
